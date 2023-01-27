8ede688f2c66ba794e1acae5bb030f170d6f67b6
rohanshetty96 <shetty.rohan83@gmail.com>
2023-01-27 09:53:25 -0600

    Add files via upload
    I have edited the config.yaml file as explained in the documentation. The error message that pops up (attached screenshot for the same) is in the code.

---
+++ types/index.ts
@@ -0,0 +1,6 @@
+import {Config} from '@verdaccio/types';
+
+export declare const REGISTRY_DOMAIN = "https://registry.npmjs.org";
+export default function loadPlugin<T>>(config: Config, pluginConfigs: any, params : any, sanity {
+  foo: string;
+}
